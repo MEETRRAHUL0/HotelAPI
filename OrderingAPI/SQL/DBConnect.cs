cf3dd39f7b0f90c796110e1c0a5fd70460fd09d2
r <meetrrahul0@gmail.com>
2021-10-03 20:26:01 +0530

    api

--- OrderingAPI/SQL/DBConnect.cs
+++ OrderingAPI/SQL/DBConnect.cs
@@ -177,28 +177,34 @@ namespace OrderingAPI.SQL
                                 res.Add(new OrderDetails
                                 {
                                     SNo = count.ToString(),  //	1
-                                    OrderID = (string)reader["RefID"],  //	3983446300
-                                    OrderDate = (string)reader["RefID"],  //	9/18/2021 11:14
+                                    OrderID = (string)reader["OrderId"],  //	3983446300
+                                    OrderDate = (string)reader["OrderDate"],  //	9/18/2021 11:14
                                     ResName = (string)reader["RefID"],  //	Royal Bakery
                                     ResID = (string)reader["RefID"],  //	102322
                                     Modeofpayment = (string)reader["RefID"],  //	Online
                                     Orderstatus = (string)reader["RefID"],  //	Delivered
                                     GrossRevenue = (string)reader["RefID"],  //	441
-                                    ProDiscountShare = (string)reader["RefID"],  //	0
-                                    CustomerCompensation = (string)reader["RefID"],  //	0
-                                    CustomerDiscountAmount = (string)reader["RefID"],  //	0
                                     CommissionableAmount = (string)reader["RefID"],  //	420
                                     CommissionPercentage = (string)reader["RefID"],  //	20
                                     CommissionValue = (string)reader["RefID"],  //	84
                                     ConvenienceFee = (string)reader["RefID"],  //	8.11
+                                    Netreceivable = (string)reader["RefID"],  //	323.91
+                                    Settlementstatus = (string)reader["RefID"],  //	pending
+                                    TaxesonZomatofees = (string)reader["RefID"],  //	16.58
+                                    Taxcollectedatsource = (string)reader["RefID"],  //	4.2
+                                    TDSamount = (string)reader["RefID"],  //	4.2
+
+
+                                    CustomerCompensation = (string)reader["RefID"],  //	0
+                                    CustomerDiscountAmount = (string)reader["RefID"],  //	0
+
+
                                     Piggybank = (string)reader["RefID"],  //	0
+                                    ProDiscountShare = (string)reader["RefID"],  //	0
                                     LogisticsCharge = (string)reader["RefID"],  //	0
                                     PenaltyAmount = (string)reader["RefID"],  //	0
                                     CreditsCharge = (string)reader["RefID"],  //	0
                                     CancellationRefund = (string)reader["RefID"],  //	0
-                                    TaxesonZomatofees = (string)reader["RefID"],  //	16.58
-                                    Taxcollectedatsource = (string)reader["RefID"],  //	4.2
-                                    TDSamount = (string)reader["RefID"],  //	4.2
                                     Amountreceivedincash = (string)reader["RefID"],  //	0
                                     Creditnoteadjustment = (string)reader["RefID"],  //	0
                                     Promorecoveryadjustment = (string)reader["RefID"],  //	0
@@ -206,8 +212,6 @@ namespace OrderingAPI.SQL
                                     Icecreamhandlingcharge = (string)reader["RefID"],  //	0
                                     Supportcostadjustment = (string)reader["RefID"],  //	0
                                     Totaladjustment = (string)reader["RefID"],  //	0
-                                    Netreceivable = (string)reader["RefID"],  //	323.91
-                                    Settlementstatus = (string)reader["RefID"],  //	pending
                                     Settlementdate = (string)reader["RefID"],  //
                                     BankUTR = (string)reader["RefID"],  //
                                 });
